d78c67de67be284fc79029b94badedb98f4e47bb
GabeHill <gabelhill2@gmail.com>
2019-02-26 14:25:31 -0700

    H

--- PenteTest/UnitTest1.cs
+++ PenteTest/UnitTest1.cs
@@ -5,7 +5,26 @@ namespace PenteTest {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+        public void TestPrintBoard() {
+            string res = Game.PrintBoard();
+            string expected =   "   a b c d e f g h i j k l m n o p q r s\n1 + + + + + + + + + + + + + + + + + + +\n2 + + + + + + + + + + + + + + + + + + +\n3 + + + + + + + + + + + + + + + + + + +\n4 + + + + + + + + + + + + + + + + + + +\n5 + + + + + + + + + + + + + + + + + + +\n6 + + + + + + + + + + + + + + + + + + +\n7 + + + + + + + + + + + + + + + + + + +\n8 + + + + + + + + + + + + + + + + + + +\n9 + + + + + + + + + + + + + + + + + + +\n10 + + + + + + + + + + + + + + + + + + +\n11 + + + + + + + + + + + + + + + + + + +\n12 + + + + + + + + + + + + + + + + + + +\n13 + + + + + + + + + + + + + + + + + + +\n14 + + + + + + + + + + + + + + + + + + +\n15 + + + + + + + + + + + + + + + + + + +\n16 + + + + + + + + + + + + + + + + + + +\n17 + + + + + + + + + + + + + + + + + + +\n18 + + + + + + + + + + + + + + + + + + +\n19 + + + + + + + + + + + + + + + + + + +";
+            
+            Assert.Equals(res, expected);
+        }
+
+
+        [TestMethod]
+        public void TestPlacePiece() {
+
+        }
+
+        [TestMethod]
+        public void TestCapturePair() {
+
+        }
+        [TestMethod]
+        public void TestWinCheckt() {
+
         }
     }
 }
