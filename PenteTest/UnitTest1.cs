ffb512149a28d58bb185ba9c0632923be2bc5286
Cfaust <cfaust@student.neumont.edu>
2019-02-26 18:44:41 -0700

    start of placeTest

--- PenteTest/UnitTest1.cs
+++ PenteTest/UnitTest1.cs
@@ -1,29 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace PenteTest {
+namespace PenteTest
+{
     [TestClass]
-    public class UnitTest1 {
+    public class UnitTest1
+    {
         [TestMethod]
-        public void TestPrintBoard() {
+        public void TestPrintBoard()
+        {
             string res = Game.PrintBoard();
-            string expected =   "   a b c d e f g h i j k l m n o p q r s\n1 + + + + + + + + + + + + + + + + + + +\n2 + + + + + + + + + + + + + + + + + + +\n3 + + + + + + + + + + + + + + + + + + +\n4 + + + + + + + + + + + + + + + + + + +\n5 + + + + + + + + + + + + + + + + + + +\n6 + + + + + + + + + + + + + + + + + + +\n7 + + + + + + + + + + + + + + + + + + +\n8 + + + + + + + + + + + + + + + + + + +\n9 + + + + + + + + + + + + + + + + + + +\n10 + + + + + + + + + + + + + + + + + + +\n11 + + + + + + + + + + + + + + + + + + +\n12 + + + + + + + + + + + + + + + + + + +\n13 + + + + + + + + + + + + + + + + + + +\n14 + + + + + + + + + + + + + + + + + + +\n15 + + + + + + + + + + + + + + + + + + +\n16 + + + + + + + + + + + + + + + + + + +\n17 + + + + + + + + + + + + + + + + + + +\n18 + + + + + + + + + + + + + + + + + + +\n19 + + + + + + + + + + + + + + + + + + +";
-            
+            string expected = "   a b c d e f g h i j k l m n o p q r s\n1 + + + + + + + + + + + + + + + + + + +\n2 + + + + + + + + + + + + + + + + + + +\n3 + + + + + + + + + + + + + + + + + + +\n4 + + + + + + + + + + + + + + + + + + +\n5 + + + + + + + + + + + + + + + + + + +\n6 + + + + + + + + + + + + + + + + + + +\n7 + + + + + + + + + + + + + + + + + + +\n8 + + + + + + + + + + + + + + + + + + +\n9 + + + + + + + + + + + + + + + + + + +\n10 + + + + + + + + + + + + + + + + + + +\n11 + + + + + + + + + + + + + + + + + + +\n12 + + + + + + + + + + + + + + + + + + +\n13 + + + + + + + + + + + + + + + + + + +\n14 + + + + + + + + + + + + + + + + + + +\n15 + + + + + + + + + + + + + + + + + + +\n16 + + + + + + + + + + + + + + + + + + +\n17 + + + + + + + + + + + + + + + + + + +\n18 + + + + + + + + + + + + + + + + + + +\n19 + + + + + + + + + + + + + + + + + + +";
+
             Assert.Equals(res, expected);
         }
 
 
         [TestMethod]
-        public void TestPlacePiece() {
+        public void TestPlacePiece()
+        {
+            string test = "abcdefghijklmnopqrs\n1+++++++++++++++++++\n2+++++++++++++++++++\n3+++++++++++++++++++\n4+++++++++++++++++++\n5+++++++++++++++++++\n6+++++++++++++++++++\n7+++++++++++++++++++\n8+++++++++++++++++++\n9+++++++++++++++++++\n10+++++++++++++++++++\n11+++++++++++++++++++\n12+++++++++++++++++++\n13+++++++++++++++++++\n14+++++++++++++++++++\n15+++++++++++++++++++\n16+++++++++++++++++++\n17+++++++++++++++++++\n18+++++++++++++++++++\n19+++++++++++++++++++";
+            char[,] res = Game.PlacePiece(0, 0, 'b'); //[x][y][piece color]
+            string[] stringArr = test.Split('\n');
+            char[][] expected = null;
 
+            Assert.Equals(res, expected);
         }
 
         [TestMethod]
-        public void TestCapturePair() {
+        public void TestCapturePair()
+        {
 
         }
         [TestMethod]
-        public void TestWinCheckt() {
+        public void TestWinCheckt()
+        {
 
         }
     }
