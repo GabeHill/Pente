9d121144e3eb97f68f9d9f05aeba391c1badbd3f
Cfaust <cfaust@student.neumont.edu>
2019-03-01 18:03:31 -0700

    placePiece unit test is complete

--- PenteProject/Game.cs
+++ PenteProject/Game.cs
@@ -229,10 +229,11 @@ namespace PenteProject
             }
         }
 
-        public void placePiece(char col, int row, char player)
+        public char[,] placePiece(char col, int row, char player)
         {
             int column = col - 97;
             board[row, column] = player;
+            return board;
         }
 
         public void createBoard()
